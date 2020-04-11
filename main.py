39b34df9f5eeedbb414c6808364b8510ebfd5658
M1k0t0 <3316945799@qq.com>
2020-04-11 15:06:18 +0800

    💩

--- main.py
+++ main.py
@@ -328,23 +328,38 @@ else:
                     connection = Image.open(res_dir+'road_EN-WS.png').convert('RGBA')
                     x0 = x - 0.5
                     y0 = y + 0.5
+                    pix_x = int(x0 * pixs)
+                    pix_y = int(y0 * pixs)
+                    tmp = bg.crop((pix_x, pix_y, pix_x + pixs, pix_y + pixs)).convert('RGBA')
+                    tmp.alpha_composite(connection)
+                    bg.paste(tmp, (pix_x, pix_y, pix_x + pixs, pix_y + pixs))
                 if road[x][y + 1] == 1:
                     connection = Image.open(res_dir+'road_N-S.png').convert('RGBA')
                     x0 = x
                     y0 = y + 0.5
+                    pix_x = int(x0 * pixs)
+                    pix_y = int(y0 * pixs)
+                    tmp = bg.crop((pix_x, pix_y, pix_x + pixs, pix_y + pixs)).convert('RGBA')
+                    tmp.alpha_composite(connection)
+                    bg.paste(tmp, (pix_x, pix_y, pix_x + pixs, pix_y + pixs))
                 if road[x + 1][y + 1] == 1:
                     connection = Image.open(res_dir+'road_WN-ES.png').convert('RGBA')
                     x0 = x + 0.5
                     y0 = y + 0.5
+                    pix_x = int(x0 * pixs)
+                    pix_y = int(y0 * pixs)
+                    tmp = bg.crop((pix_x, pix_y, pix_x + pixs, pix_y + pixs)).convert('RGBA')
+                    tmp.alpha_composite(connection)
+                    bg.paste(tmp, (pix_x, pix_y, pix_x + pixs, pix_y + pixs))
                 if road[x + 1][y] == 1:
                     connection = Image.open(res_dir+'road_W-E.png').convert('RGBA')
                     x0 = x + 0.5
                     y0 = y 
-                pix_x = int(x0 * pixs)
-                pix_y = int(y0 * pixs)
-                tmp = bg.crop((pix_x, pix_y, pix_x + pixs, pix_y + pixs)).convert('RGBA')
-                tmp.alpha_composite(connection)
-                bg.paste(tmp, (pix_x, pix_y, pix_x + pixs, pix_y + pixs))
+                    pix_x = int(x0 * pixs)
+                    pix_y = int(y0 * pixs)
+                    tmp = bg.crop((pix_x, pix_y, pix_x + pixs, pix_y + pixs)).convert('RGBA')
+                    tmp.alpha_composite(connection)
+                    bg.paste(tmp, (pix_x, pix_y, pix_x + pixs, pix_y + pixs))
         print()
 
     res_dir = './img/'
